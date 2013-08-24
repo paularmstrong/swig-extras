bdb24de73285a02617af32b9eb95014ce1e72b4a
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-08-24 15:23:22 -0700

    filter: markdown

---
+++ index.js
@@ -0,0 +1,2 @@
+exports.tags = require('./lib/tags');
+exports.filters = require('./lib/filters');
---
+++ tests/filters.test.js
@@ -0,0 +1,15 @@
+var swig = require('swig'),
+  expect = require('expect.js'),
+  extras = require('../');
+
+describe('Filters:', function () {
+
+  describe('markdown', function () {
+    swig.setFilter('markdown', extras.filters.markdown);
+    it('{{ foo|markdown|raw }}', function () {
+      expect(swig.render('{{ foo|markdown|raw }}', { locals: { foo: '# This is an H1' }}))
+        .to.equal('<h1>This is an H1</h1>');
+    });
+  });
+
+});
